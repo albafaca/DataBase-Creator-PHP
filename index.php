85914621256e6a4b0aa152b7822aaa7876b27904
albafaca <facundo.abassini@gmail.com>
2015-03-30 16:40:52 -0300

    index.php
    The main file.

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+
+function __autoload($classname) {
+    $filename = "./class/". $classname .".php";
+    include_once($filename);
+}
+
+
+$NewBase=new creador();
+
+$NewBase->host = "localhost";
+$NewBase->user = "root";
+$NewBase->pass = "";
+$NewBase->DBname = "Proyecto";
+
+$NewBase->DBCreator();
+
+?>
